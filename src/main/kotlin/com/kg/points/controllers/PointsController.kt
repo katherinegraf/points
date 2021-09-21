6d1799c13ebbfcc10fa0c5c3df0c7c5f98aaea58
Katherine Graf <katherinelgraf@gmail.com>
2021-09-21 14:49:28 -0500

    return points balances by payer

--- src/main/kotlin/com/kg/points/controllers/PointsController.kt
+++ src/main/kotlin/com/kg/points/controllers/PointsController.kt
@@ -5,6 +5,7 @@ import com.kg.points.repos.PointsRepo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -17,6 +18,20 @@ class PointsController {
 
     @Autowired
     private lateinit var pointsRepo: PointsRepo
+
+    @GetMapping("/pointsBalance")
+    fun getPointsBalances(): ResponseEntity<MutableMap<String, Int>> {
+        val pointsBalance = mutableMapOf<String, Int>()
+        val activePoints = pointsRepo.findAll()
+        activePoints.forEach{
+            if (it.payer !in pointsBalance.keys) {
+                pointsBalance[it.payer] = it.points
+            } else {
+                pointsBalance[it.payer] = it.points + pointsBalance.getValue(it.payer)
+            }
+        }
+        return ResponseEntity(pointsBalance, HttpStatus.OK)
+    }
 
     @PostMapping("/addTransaction")
     fun addTransaction(
